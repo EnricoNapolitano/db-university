f00c78f73384a583f95eaeee5b5cb6c04c188d35
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-02-15 15:43:38 +0100

    SQL Queries

---
+++ ex-query-con-join.sql
@@ -0,0 +1,58 @@
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia 
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` AS 'faculty'
+FROM `students`
+JOIN `degrees`
+ON `degrees`.`id` = `students`.`degree_id`
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze 
+SELECT `degrees`.`name` AS 'faculties', `departments`.`name` AS 'departments'
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44) 
+SELECT `teachers`.`name`, `teachers`.`surname`, `courses`.`name` AS 'class'
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.`id` = 44;
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome 
+SELECT  `students`.`surname`, `students`.`name`, `degrees`.*, `departments`.`name` AS 'department'
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+JOIN `students`
+ON `degrees`.`id` = `students`.`degree_id`
+ORDER BY
+`students`.`surname`  ASC,
+`students`.`name` ASC;
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `degrees`.`name` AS 'faculties', `courses`.`name` AS 'courses', `teachers`.`name`, `teachers`.`surname`
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`;
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54) 
+SELECT DISTINCT `teachers`.`name`, `teachers`.`surname`, `departments`.`name` AS 'department'
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `degrees`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica';
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
